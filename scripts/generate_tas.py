be36f100522a63c6be115ba17108323adc8a2b22
Peter Bui <pbui@nd.edu>
2021-02-07 15:30:19 -0500

    generate_tas: factor in conflict of interests

--- scripts/generate_tas.py
+++ scripts/generate_tas.py
@@ -3,18 +3,34 @@
 import csv
 import random
 import glob
+import sys
 import yaml
 
 TAS      = [ta['github'] for ta in yaml.safe_load(open('static/yaml/ta.yaml'))]
 STUDENTS = []
 
-for student in csv.DictReader(open('static/csv/students.csv', 'rU')):
+for student in csv.DictReader(open('static/csv/students.csv', 'r')):
     STUDENTS.append(student['Netid'])
 
-random.shuffle(STUDENTS)
-random.shuffle(TAS)
+CONFLICTS = {
+    ta['github']: ta['conflicts']
+    for ta in yaml.safe_load(open('static/yaml/ta.yaml'))
+    if ta.get('conflicts')
+}
 
-TAS      = TAS * (len(STUDENTS) // len(TAS) + 1)
-MAPPING  = list(sorted(map(list, zip(STUDENTS, TAS))))
+has_conflict = True
+
+while has_conflict:
+    random.shuffle(STUDENTS)
+    random.shuffle(TAS)
+
+    TAS          = TAS * (len(STUDENTS) // len(TAS) + 1)
+    MAPPING      = sorted(map(list, zip(STUDENTS, TAS)))
+    has_conflict = False
+
+    for ta, conflicts in CONFLICTS.items():
+        for conflict in conflicts:
+            if [conflict, ta] in MAPPING:
+                has_conflict = True
 
 print(yaml.dump(MAPPING, default_flow_style=False))
